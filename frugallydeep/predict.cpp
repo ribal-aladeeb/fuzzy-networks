d23e6bd0aceb705a9d5ed7ddc15acd5defd958f5
Ribal Aladeeb <aladeeb.r@hotmail.com>
2020-08-06 22:12:59 -0400

    Make prediction using the actual image values
    This is likely to be the wrong format as the vector input contains all red
    values, followed by all green and all blue pixel values.

--- frugallydeep/predict.cpp
+++ frugallydeep/predict.cpp
@@ -4,42 +4,47 @@ using namespace std;
 #include <fstream>
 #include <iostream>
 
-int readOneImage()
-{
+
+vector<float> readImageInColourOrder() {
+    // All red pixel values first, then green, then blue
+
     string filename = "cifar-10-batches-bin/data_batch_1.bin";
     ifstream dataFile(filename, std::ifstream::binary);
     char bucket;
-    dataFile.get(bucket);
-    int imageClass = (int)bucket;
-    std::cout << imageClass << std::endl;
-    char line[32];
-    dataFile.read(line, 32);
-    float pixel[32];
-    for (int i = 0; i < 32; i++)
+    dataFile.read(&bucket, 1);
+    float imageClass = (float)(uint8_t)bucket;
+    std::cout <<"image class "<< imageClass << std::endl;
+    
+    // seek the first pixel value
+    dataFile.seekg(1);
+    
+    char bytes[3072];
+    dataFile.read(bytes, 3072);
+
+    vector<float> image;
+
+    float pixelValue;
+
+    for (int i=0; i<3072; i++)
     {
-        pixel[i] = (float)line[i];
-        std::cout << pixel[i] << std::endl;
+        pixelValue = ((float)(uint8_t)bytes[i])/255;
+        image.push_back(pixelValue);
     }
+
+    image.push_back(imageClass);
+
     dataFile.close();
-}
 
-vector<vector<vector<float>>> generateImage()
-{
-
-    vector<vector<vector<float>>> image(32, vector<vector<float>>(32, vector<float>(3, 0)));
-    for (int i = 0; i < 32; i++)
-    {
-        for (int j = 0; j < 32; j++)
-        {
-            for (int k = 0; k < 3; k++)
-            {
-                // this simulates the pixel value range used to train the model
-                image[i][j][k] = float(std::rand() % 255 / 255.0);
-            }
-        }
-    }
     return image;
 }
+
+// TODO
+// vector<float> readImageAlternateColours(){
+//     // red, green, blue pixel value of each pixel
+//     return null
+// }
+
+
 vector<float> generate1Dimage()
 {
     vector<float> image(32 * 32 * 3);
@@ -52,10 +57,15 @@ vector<float> generate1Dimage()
 
 int main()
 {
-    const auto image = generate1Dimage();
+    vector<float> image = readImageInColourOrder();
+    float imageClass  = image[3073];
+    image.pop_back();
+    cout << imageClass<<"__"<< image.size() <<std::endl;
+
     const auto model = fdeep::load_model("fdeep_model.json");
 
-    const auto result = model.predict({fdeep::tensor(fdeep::tensor_shape(
-        static_cast<std::size_t>(32),static_cast<std::size_t>(32),static_cast<std::size_t>(3)), image)});
+    const auto result = model.predict({ fdeep::tensor(fdeep::tensor_shape(
+        static_cast<std::size_t>(32), static_cast<std::size_t>(32), static_cast<std::size_t>(3)), image) });
+    std::cout << std::setprecision(16); // show 16 digits of precision
     std::cout << fdeep::show_tensors(result) << std::endl;
 }
