10d73c26aa5b9fc49459b873fa43312a22f3a132
Ribal Aladeeb <aladeeb.r@hotmail.com>
2020-08-11 22:06:42 -0400

    Implement softmax on the raw predictions

--- frugallydeep/predict.cpp
+++ frugallydeep/predict.cpp
@@ -4,8 +4,8 @@ using namespace std;
 #include <fstream>
 #include <iostream>
 
-
-vector<float> readImageInColourOrder() {
+vector<float> readImageInColourOrder()
+{
     // All red pixel values first, then green, then blue
 
     string filename = "cifar-10-batches-bin/data_batch_1.bin";
@@ -13,7 +13,7 @@ vector<float> readImageInColourOrder() {
     char bucket;
     dataFile.read(&bucket, 1);
     float imageClass = (float)(uint8_t)bucket;
-    std::cout <<"image class "<< imageClass << std::endl;
+    std::cout << "image class " << imageClass << std::endl;
 
     // seek the first pixel value
     dataFile.seekg(1);
@@ -25,9 +25,9 @@ vector<float> readImageInColourOrder() {
 
     float pixelValue;
 
-    for (int i=0; i<3072; i++)
+    for (int i = 0; i < 3072; i++)
     {
-        pixelValue = ((float)(uint8_t)bytes[i])/255;
+        pixelValue = ((float)(uint8_t)bytes[i]) / 255;
         image.push_back(pixelValue);
     }
 
@@ -38,40 +38,71 @@ vector<float> readImageInColourOrder() {
     return image;
 }
 
-
-vector<float> readImageAlternateColours() {
+vector<float> readImageAlternateColours()
+{
     // return the image with red, green, blue pixel value of each pixel.
     vector<float> image = readImageInColourOrder();
     vector<float> newImage;
-    for (int i=0; i<3072;i++)
+    for (int i = 0; i < 3072; i++)
     {
         int offset; //red values need no offset
         if (i % 3 == 0)
-            offset=0;
+            offset = 0;
         else if (i % 3 == 2) // green values come after red
             offset = 1024;
         else if (i % 3 == 2) // blue values come last
             offset = 2048;
 
-        newImage.push_back(image[i+offset]);
+        newImage.push_back(image[i + offset]);
     }
     // After adding all pixels add the image class to end of vector
-    newImage.push_back(image[image.size()-1]);
-    
+    newImage.push_back(image[image.size() - 1]);
+
     return newImage;
+}
+
+vector<float> softmax(vector<float> rawPredictions)
+{
+    vector<float> exponents;
+    float sumExpo;
+
+    for (int i = 0; i < rawPredictions.size(); i++)
+    {
+        exponents.push_back(exp(rawPredictions[i]));
+        sumExpo += exponents[i];
+    }
+
+    vector<float> softmaxed;
+
+    for (int i = 0; i < exponents.size(); i++)
+    {
+        softmaxed.push_back(exponents[i] / sumExpo);
+    }
+
+    return softmaxed;
 }
 
 int main()
 {
     vector<float> image = readImageAlternateColours();
-    float imageClass  = image[3072];
+    float imageClass = image[3072];
     image.pop_back();
-    cout << imageClass<<"__"<< image.size() <<std::endl;
 
     const auto model = fdeep::load_model("fdeep_model.json");
 
-    const auto result = model.predict({ fdeep::tensor(fdeep::tensor_shape(
-        static_cast<std::size_t>(32), static_cast<std::size_t>(32), static_cast<std::size_t>(3)), image) });
-    std::cout << std::setprecision(16); // show 16 digits of precision
-    std::cout << fdeep::show_tensors(result) << std::endl;
+    fdeep::tensors result = model.predict({fdeep::tensor(
+        fdeep::tensor_shape(
+            static_cast<std::size_t>(32),
+            static_cast<std::size_t>(32),
+            static_cast<std::size_t>(3)),
+        image)});
+
+    vector<float> t = result.at(0).to_vector();
+
+    vector<float> probabilities = softmax(t);
+
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << probabilities[i] << std::endl;
+    }
 }
